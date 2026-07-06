ec786669c234a874875766c732dd8f965a011f88
RohitGrandMaster <hyperhacking01@gmail.com>
2026-07-06 10:36:15 +0530

    chore: setup production stack, docker, and fix gitignore rules

--- library-backend/dist/users/users.service.d.ts
+++
@@ -1,7 +0,0 @@
-import { Repository } from 'typeorm';
-import { User } from './entities/user.entity';
-export declare class UsersService {
-    private userRepo;
-    constructor(userRepo: Repository<User>);
-    findOne(id: string): Promise<User | null>;
-}
